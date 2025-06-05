a36477a858b1075ae1b6990e4e18b9c6c5018b06
Bethany Burrows <jedcburrows@gmail.com>
2025-06-05 14:03:47 +0100

    Added validation function to confirm csv was successfully converted to json

--- csv_functions.py
+++ csv_functions.py
@@ -1,22 +1,24 @@
 import csv
 import pandas as pd
 from pathlib import Path
+from json_functions import read_json
 
 BASE_DIR = Path("CSV_Files")
 res = BASE_DIR / "test_csv.csv"
 
-with open(res, "r", encoding="utf-8") as file:
-    csv_reader = csv.reader(file, delimiter=",")
-    line_num = 0
-    for row in csv_reader:
-        if line_num == 0:
-            print(f"The column names are " + ",".join(row))
-            line_num += 1
-            category_names = row
-        else:
-            zpd = zip(category_names, row)
-            for r in zpd:
-                print(f"{r[0]}: {r[1]}")
+def read_csv(f:Path):
+    with open(res, "r", encoding="utf-8") as file:
+        csv_reader = csv.reader(file, delimiter=",")
+        line_num = 0
+        for row in csv_reader:
+            if line_num == 0:
+                print(f"The column names are " + ",".join(row))
+                line_num += 1
+                category_names = row
+            else:
+                zpd = zip(category_names, row)
+                for r in zpd:
+                    print(f"{r[0]}: {r[1]}")
 
 def convert_to_json(f:Path) -> None:
     #Generate new path
@@ -26,3 +28,28 @@ def convert_to_json(f:Path) -> None:
     temp = pd.read_csv(f)
     temp.to_json(new_path, orient='records', force_ascii=False, indent=1)
 
+def validate_json_conversion(p1: Path, p2: Path):
+    js = read_json(p1)
+    with open(p2, "r", encoding="utf-8") as file:
+        csv_reader = csv.reader(file, delimiter=",")
+        jcats = [k for k in js[0].keys()]
+        for i, row in enumerate(csv_reader):
+            if i == 0:
+                if row == jcats:
+                    print("Categories ok")
+                else:
+                    print("No match")
+                    return False
+            else:
+                jrow = [str(v) for v in js[i-1].values()]
+                print(row)
+                print(jrow)
+                if row == jrow:
+                    print("Categories ok")
+                else:
+                    print("No match")
+                    return False
+        return True
+
+p = res.parent.parent / 'JSON_Files' / f'{res.stem}.json'
+print(f'JSON matches CSV? {validate_json_conversion(p,res)}')
