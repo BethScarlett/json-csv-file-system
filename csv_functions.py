d651f22a505478afbac55251a9cd2b2c6794d07e
Bethany Burrows <jedcburrows@gmail.com>
2025-06-05 15:52:46 +0100

    Temporarily updated converted json path to store file in same location as converter. Added logging to fix validation bug.

--- csv_functions.py
+++ csv_functions.py
@@ -1,4 +1,5 @@
 import csv
+import logging
 import pandas as pd
 from pathlib import Path
 from json_functions import read_json
@@ -22,7 +23,7 @@ def read_csv(f:Path):
 
 def convert_to_json(f:Path) -> bool:
     #Generate new path
-    new_path = f.parent.parent / 'JSON_Files' / f'{f.stem}.json'
+    new_path = Path(f'{f.stem}.json')
 
     #Read file and convert to JSON using new path
     temp = pd.read_csv(f)
@@ -31,22 +32,39 @@ def convert_to_json(f:Path) -> bool:
     return validate_json_conversion(new_path, f)
 
 def validate_json_conversion(p1: Path, p2: Path):
+    config_logger()
     js = read_json(p1)
     with open(p2, "r", encoding="utf-8") as file:
         csv_reader = csv.reader(file, delimiter=",")
         jcats = [k for k in js[0].keys()]
         for i, row in enumerate(csv_reader):
+            logging.debug(f'CSV Row - {row}')
+            logging.debug(f'JSON Row - {jcats}')
             if i == 0:
                 if row != jcats:
+                    logging.debug(f"{row} doesn't equal {jcats}")
                     return False
             else:
                 jrow = [str(v) for v in js[i-1].values()]
                 cleanrow = []
                 for v in row:
-                    if v == 'false' or 'true':
+                    if v == 'false' or v == 'true':
+                        logging.info(f'Converting {v} to {v.title()}')
                         v = v.title()
                     cleanrow.append(v)
 
+                logging.debug(f'CSV Row - {cleanrow}')
+                logging.debug(f'JSON Row - {jrow}')
+
                 if cleanrow != jrow:
+                    logging.debug(f"{cleanrow} doesn't equal {jrow}")
                     return False
         return True
+
+def config_logger():
+    logging.basicConfig(level=0,                                            #Level to start logging at
+                        filename="log.txt",                                 #Name of file to write log to
+                        filemode="w",                                       #Filemode to open file in (Note: 'w' will create file if it doesn't exist)
+                        format="{asctime} - {levelname} - {message}",       #Format string to desired look (asctime = current time & date, more can be found in python docs)
+                        style="{"                                           #Define style for formatting
+                        )
